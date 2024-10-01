9b0b1042cd70f8b464eed22a2eb5396751a423cf
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-01 14:20:26 -0500

    feat(init): Initial commit
    Added flutter project with very good cli

---
+++ android/app/src/main/kotlin/co/aulaabierta/practicame/practicame/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.aulaabierta.practicame.practicame_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:practicame_app/app/app.dart';
+import 'package:practicame_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
