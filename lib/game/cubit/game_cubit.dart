3025b84fae648b1fca961978ce2d07fd697e1e89
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-07 17:29:30 -0500

    wip

--- lib/game/cubit/game_cubit.dart
+++ lib/game/cubit/game_cubit.dart
@@ -1,8 +1,93 @@
 import 'package:bloc/bloc.dart';
 
-class GameCubit extends Cubit<int> {
-  GameCubit() : super(0);
+class GameCubit extends Cubit<GameState> {
+  GameCubit(String correctAnswer)
+      : super(
+          GameState(
+            correctAnswer: correctAnswer,
+            currentInput: List.filled(
+              correctAnswer.length,
+              '',
+            ), // Lista vacía para las letras ingresadas
+            stars: List.filled(
+              correctAnswer.length,
+              false,
+            ), // Falsos inicialmente, indicando que ninguna letra es correcta
+          ),
+        );
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
+  // Actualiza la letra ingresada en la posición correcta
+  void updateLetter(int index, String letter) {
+    // Verificar que la entrada sea válida
+    if (letter.isEmpty) return;
+
+    final updatedInput = List<String>.from(
+        state.currentInput); // Crear una copia de la lista actual
+    updatedInput[index] =
+        letter; // Actualizar la letra en la posición correspondiente
+
+    final updatedStars = List<bool>.from(
+        state.stars); // Crear una copia del estado de las estrellas
+
+    // Verificamos si la letra ingresada es correcta (comparando ignorando mayúsculas)
+    final correctLetter = state.correctAnswer[index].toLowerCase();
+    final inputLetter = letter.toLowerCase();
+
+    if (inputLetter == correctLetter) {
+      print('La letra es correcta: $inputLetter en la posición $index');
+      updatedStars[index] =
+          true; // Si es correcta, actualizamos la estrella a true (verde)
+    } else {
+      print('La letra es incorrecta: $inputLetter en la posición $index');
+      updatedStars[index] =
+          false; // Si es incorrecta, la estrella es false (rojo)
+    }
+
+    // Emitir el nuevo estado con el input y las estrellas actualizadas
+    emit(state.copyWith(currentInput: updatedInput, stars: updatedStars));
+  }
+
+  // Resetea el estado del juego
+  void reset() {
+    emit(
+      GameState(
+        correctAnswer: state.correctAnswer,
+        currentInput: List.filled(
+          state.correctAnswer.length,
+          '',
+        ), // Volver a un input vacío
+        stars: List.filled(
+          state.correctAnswer.length,
+          false,
+        ), // Reseteamos las estrellas
+      ),
+    );
+  }
+}
+
+class GameState {
+  // Estado de las estrellas (correcto o incorrecto para cada letra)
+  GameState({
+    required this.correctAnswer,
+    required this.currentInput,
+    required this.stars,
+  });
+
+  final String correctAnswer; // Respuesta correcta
+  final List<String> currentInput; // Letras ingresadas por el usuario
+  final List<bool>
+      stars; // Lista de booleanos que indican si cada letra es correcta
+
+  // Método para copiar el estado actual y modificar solo los campos necesarios
+  GameState copyWith({
+    String? correctAnswer,
+    List<String>? currentInput,
+    List<bool>? stars,
+  }) {
+    return GameState(
+      correctAnswer: correctAnswer ?? this.correctAnswer,
+      currentInput: currentInput ?? this.currentInput,
+      stars: stars ?? this.stars,
+    );
+  }
 }
--- lib/game/view/game_page.dart
+++ lib/game/view/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:practicame_app/app/app.dart';
 import 'package:practicame_app/game/cubit/game_cubit.dart';
@@ -11,7 +12,7 @@ class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => GameCubit(),
+      create: (_) => GameCubit(gameInput.answer),
       child: const GameView(),
     );
   }
@@ -63,11 +64,14 @@ class GameBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GameImage(image: gameInput.pictogramImage),
-        GameQuestion(question: gameInput.question),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          GameImage(image: gameInput.pictogramImage),
+          GameQuestion(question: gameInput.question),
+          const GameAnswerInputCol(),
+        ],
+      ),
     );
   }
 }
@@ -89,5 +93,181 @@ class GameQuestion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(question);
+  }
+}
+
+class GameAnswerInputCol extends StatefulWidget {
+  const GameAnswerInputCol({super.key});
+
+  @override
+  _GameAnswerInputColState createState() => _GameAnswerInputColState();
+}
+
+class _GameAnswerInputColState extends State<GameAnswerInputCol> {
+  // Lista de controladores para cada letra
+  late List<TextEditingController> _controllers;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeControllers();
+  }
+
+  // Inicializar controladores basados en la longitud de la respuesta correcta
+  void _initializeControllers() {
+    final correctAnswer = context.read<GameCubit>().state.correctAnswer;
+    _controllers =
+        List.generate(correctAnswer.length, (_) => TextEditingController());
+  }
+
+  @override
+  void dispose() {
+    // Liberar los controladores cuando el widget sea destruido
+    for (final controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GameCubit, GameState>(
+      builder: (context, state) {
+        final words = state.correctAnswer.split(' ');
+        var globalIndex = 0; // Índice global para recorrer todas las letras
+
+        return Column(
+          children: words.map((word) {
+            // Construimos la fila de texto para cada palabra
+            final row = Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: word.characters.map((letter) {
+                final currentIndex = globalIndex;
+                globalIndex++; // Aumentamos el índice global después de cada letra
+
+                return SizedBox(
+                  width: 50,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _controllers[currentIndex],
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: state.stars[currentIndex]
+                          ? Colors.green.shade100
+                          : Colors.red.shade100,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    maxLength: 1,
+                    onChanged: (value) {
+                      if (value.isNotEmpty) {
+                        // Actualizar la letra en el cubit
+                        context
+                            .read<GameCubit>()
+                            .updateLetter(currentIndex, value);
+
+                        // Mover el foco al siguiente campo si se ingresa un carácter
+                        FocusScope.of(context).nextFocus();
+                      } else {
+                        // Mover el foco al campo anterior si se borra un carácter
+                        context
+                            .read<GameCubit>()
+                            .updateLetter(currentIndex, '');
+
+                        FocusScope.of(context).previousFocus();
+                      }
+                    },
+                    style: const TextStyle(fontSize: 24),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                );
+              }).toList(), // Termina el mapeo de las letras
+            );
+
+            globalIndex++; // Incrementar para contar el espacio entre palabras
+            return row;
+          }).toList(), // Termina el mapeo de las palabras
+        );
+      },
+    );
+  }
+}
+
+class GameAnswerInput extends StatelessWidget {
+  const GameAnswerInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GameCubit, GameState>(
+      builder: (context, state) {
+        // Dividir la respuesta en palabras
+        final words = state.correctAnswer.split(' ');
+
+        return Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 8, // Espacio horizontal entre cuadros
+          runSpacing: 8, // Espacio vertical si se distribuyen en varias líneas
+          children: [
+            for (int wordIndex = 0; wordIndex < words.length; wordIndex++) ...[
+              for (int letterIndex = 0;
+                  letterIndex < words[wordIndex].length;
+                  letterIndex++)
+                SizedBox(
+                  width: 50, // Ancho fijo para cada cuadro de texto
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: state.stars[letterIndex]
+                          ? Colors.green.shade100
+                          : Colors.red.shade100,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            const BorderSide(color: Colors.grey, width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            const BorderSide(color: Colors.blue, width: 2),
+                      ),
+                    ),
+                    maxLength: 1, // Solo un carácter por cuadro
+                    onChanged: (value) {
+                      if (value.isNotEmpty) {
+                        context
+                            .read<GameCubit>()
+                            .updateLetter(letterIndex, value);
+                        FocusScope.of(context)
+                            .nextFocus(); // Mover al siguiente campo automáticamente
+                      }
+                    },
+                    style: const TextStyle(fontSize: 24),
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
+              if (wordIndex < words.length - 1)
+                const SizedBox(
+                  width: 20, // Esto solo agrega espacio visual entre palabras
+                ),
+            ],
+          ],
+        );
+      },
+    );
   }
 }
